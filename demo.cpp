7543f38e8452d4ed68208a8a968137f1d9d01339
FS <80700012+imFS@users.noreply.github.com>
2021-03-16 17:03:13 +0200

    Add files via upload

---
+++ demo.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "SecHandler.h"
+
+
+int main()
+{
+	std::cout << "Test..." << std::endl;
+
+	SecHandler handler;
+
+	// Setup filters
+	handler.on("PM", [](std::string p) {
+		std::cout << "PM Detected!" << std::endl;
+		});
+	handler.add_filter("PM", "pm ");
+
+	handler.on("GM", [](std::string p) {
+		std::cout << "GM Detected!" << std::endl;
+		});
+	handler.add_filter("GM", "gmjoined ");
+
+	// Some random strings to trigger them
+	handler.search("Hello world!");
+	handler.search("pm Hi");
+	handler.search("gmjoined HiThere");
+
+	system("pause");
+	return 0;
+}
